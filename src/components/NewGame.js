0d48660ada6c9d18733a96de2f35b891574cba10
Hagar Shilo <strayblues@gmail.com>
2020-03-26 20:40:57 +0200

    Design intro msg for mobile devices

--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -9,28 +9,35 @@ const NewGame = ({ gameStart, setGameStart }) => {
   };
   return (
     <Container className={gameStart ? "hide" : "show"}>
-      <h2>בוקר טוב, ראש הממשלה.</h2>
-      <div>
-        מבין 8.7 מיליון תושבים, ידועים לנו כרגע 7 חולי קורונה שחזרו מספינת
-        הדיאמונד פרינסס. אנחנו ממתינים להוראותיך כיצד להתקדם. בכל סיבוב תוכל
-        לבצע פעולה אחת, ותקבל הודעות מגורמים שונים במערכת הפוליטית ומחוץ לה. האם
-        תצליח לנצח את המגיפה בלי שהמדינה תקרוס?
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title">בוקר טוב, ראש הממשלה.</h5>
+        </div>
+        <div className="modal-body">
+          <p>
+            מבין 8.7 מיליון תושבים, ידועים לנו כרגע 7 חולי קורונה שחזרו מספינת
+            הדיאמונד פרינסס. אנחנו ממתינים להוראותיך כיצד להתקדם.
+          </p>
+          <p>
+            בכל סיבוב תוכל לבצע פעולה אחת, ותקבל הודעות מגורמים שונים במערכת
+            הפוליטית ומחוץ לה.
+          </p>
+          <p>האם תצליח לנצח את המגיפה בלי שהמדינה תקרוס?</p>
+        </div>
+        <div className="modal-footer">
+          <button
+            type="button"
+            className="btn btn-purple"
+            onClick={handleGameStart}
+          >
+            בוא נגלה!
+          </button>
+        </div>
       </div>
-      <button onClick={handleGameStart}>בוא נגלה!</button>
     </Container>
   );
 };
 
 export default NewGame;
 
-const Container = styled.div`
-  font-size: 90%;
-  height: 100%;
-  width: 100%;
-  text-align: center;
-
-  @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-device-pixel-ratio: 3) {
-    height: 37px;
-  }
-`;
-const Content = styled.div``;
+const Container = styled.div``;
